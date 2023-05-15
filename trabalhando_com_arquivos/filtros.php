26a5061bf27c73cd00c6e8db5e1589b421ff49a8
adilsonads <71649393+adilsonads@users.noreply.github.com>
2023-05-15 02:54:00 -0300

    Delete trabalhando_com_arquivos directory

--- trabalhando_com_arquivos/filtros.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$arquivoNomes = fopen('arquivo3.txt', 'r'); //Função que abre o arquivo
-
-stream_filter_append($arquivoNomes, 'string.toupper'); //função para criar filtros no arquivo
-
-echo fread($arquivoNomes, filesize('arquivo3.txt'));
--- trabalhando_com_arquivos/pegandoDeSites.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$arquivo = file_get_contents('teste.html');//Lê todo o conteúdo de um arquivo para uma string
-
-echo $arquivo;
